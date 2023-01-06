f48cfaac1d03bf895ef97374efa2952ce35ed4d5
JuliusBendt <julius@juto.dk>
2023-01-06 13:20:58 +0100

    Added search filters - main search is debounced

--- src/components/service/ui/filters/index.js
+++ src/components/service/ui/filters/index.js
@@ -1,5 +1,6 @@
 import dateFilter from "@/components/service/ui/filters/dateFilter.vue";
 import numberFilter from "@/components/service/ui/filters/numberFilter.vue";
+import stringFilter from "@/components/service/ui/filters/stringFilter.vue";
 import baseFilter from "@/components/service/ui/filters/baseFilter.vue";
 
-export { dateFilter, numberFilter, baseFilter };
+export { dateFilter, numberFilter, stringFilter, baseFilter };
---
+++ src/components/service/ui/filters/stringFilter.vue
@@ -0,0 +1,27 @@
+<template>
+  <baseFilter
+    :choices="[
+      {
+        title: 'Equals',
+        value: '=',
+      },
+    ]"
+    multiFieldChoice="Between"
+    selectText="Must be"
+    firstInputText="Enter text..."
+    secondInputText=""
+    :validationRules="rules"
+    :filterKey="props.filterKey"
+  />
+</template>
+
+<script setup>
+import { baseFilter } from "@/components/service/ui/filters";
+
+const props = defineProps(["filterKey"]);
+
+const rules = [
+  (value) => !!value || "Value is required",
+  (value) => (value || "").length >= 4 || "Must be 4 characters",
+];
+</script>
